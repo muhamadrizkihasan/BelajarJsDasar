4a18497e43b7320d6ec459ed9df25700ff7a18e9
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-10-12 05:06:34 +0700

    Menambahkan folder object

---
+++ 17.Object/script.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : "Ujang",
+    umur : 16,
+    ips : [3.00, 2.50, 3.20],
+    alamat : {
+        jalan : "Jl. abc No. 123",
+        kota : "Bandung",
+        provinsi : "Jawa Barat"
+    }
+};
